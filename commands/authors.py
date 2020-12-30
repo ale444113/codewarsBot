1375126aa38932605e22201931c97ec888825c79
imTDB <32682095+imTDB@users.noreply.github.com>
2020-12-30 16:56:05 -0300

    Update authors.py
    Added hyperlinks to devs githubs and also some cool markdown

--- commands/authors.py
+++ commands/authors.py
@@ -1,8 +1,8 @@
 import discord
 async def authors(message,args):
-    embed_authors = discord.Embed(title="🐧 About us 🐈", description=f"Hello {message.author.name} here is some info about us", color=0x00FFFF)
-    embed_authors.add_field(name="ale444113", value="Something", inline=False)
-    embed_authors.add_field(name="imTDB", value="Something", inline=False)
-    embed_authors.add_field(name="Mariovlv", value="Something", inline=False)
-    embed_authors.set_footer(text="Thanks for reading this <3")
+    embed_authors = discord.Embed(title="🐧 About us 🐈", description=f"Hello {message.author.name} here is some info about the devs!", color=0x00FFFF)
+    embed_authors.add_field(name="[ale444113](https://github.com/ale444113)", value="```A 14 year old programmer from Spain that enjoys making software.```", inline=False)
+    embed_authors.add_field(name="[imTDB](https://github.com/imTDB)", value="```CS Student and attempt of techtuber```", inline=False)
+    embed_authors.add_field(name="[Mariovlv](https://github.com/Mariovlv)", value="```Python programming ?```", inline=False)
+    embed_authors.set_footer(text="Thanks for reading this ❤️")
     await message.channel.send(embed=embed_authors)
