d139a74fd6422af49c43ed7d3c113753b9b6284a
imTDB <32682095+imTDB@users.noreply.github.com>
2020-12-30 18:17:41 -0300

    Update github.py
    Added a link to the project's repository

--- commands/github.py
+++ commands/github.py
@@ -1,9 +1,6 @@
 import discord
 async def github(message,args):
-    embed_github = discord.Embed(title="💫 Our githubs 💫", description=f"{message.author.name} here is our githubs!", color=0xFF00FF)
-    embed_github.add_field(name="Project github", value="https://github.com/ale444113/codewarsBot", inline=False)
-    embed_github.add_field(name="ale444113's github", value="https://github.com/ale444113", inline=False)
-    embed_github.add_field(name="imTDB's github", value="https://github.com/imTDB", inline=False)
-    embed_github.add_field(name="Mariovlv's github", value="https://github.com/Mariovlv", inline=False)
-    embed_github.set_footer(text="Thanks for reading this <3")
+    embed_github = discord.Embed(title="Project's Repository", color=0xFF00FF)
+    embed_github.add_field(name="Source Code", value="https://github.com/ale444113/codewarsBot", inline=False)
+    embed_github.set_footer(text="Give us a star ⭐")
     await message.channel.send(embed=embed_github)
