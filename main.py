ca4fb3ed2e559e68d21a4590ab5603b8ccfbe6ea
Ale <alemolinahernandez06@gmail.com>
2020-12-30 22:35:11 +0100

    Añadido status

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ async def on_ready():
     for guild in client.guilds:
         print(f"I'm in {guild.name} (id: {guild.id})")
         n_servers+=1
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=f" codewars.com | Active in {n_servers}"))#Set the presence
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=f" codewars.com | Active in {n_servers} servers"))#Set the presence
 
 @client.event
 async def on_guild_join(guild):
@@ -33,12 +33,12 @@ async def on_guild_join(guild):
     embedVar = discord.Embed(title="Say hi!", description="codewarsBot has joined the server", color=0x0000FF)
     embedVar.add_field(name="Prefix", value=f"My prefix is {PREFIX} 😃", inline=False)
     embedVar.add_field(name="Get help", value=f"Use {PREFIX}help to see a list of commands n.n", inline=False)
+    embedVar.add_field(name="Invite me", value=f"Use {PREFIX}invite to invite me to your server", inline=False)
     embedVar.set_image(url="https://jungladigital.com/wp-content/uploads/2019/03/codewars-800-350.png")
-    embedVar.set_footer(text="Made by: ale444113#6621")
 
     print(f"I just joined {guild.name} (id: {guild.id})")
     n_servers += 1
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=f" codewars.com | Active in {n_servers}"))#Set the presence again
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=f" codewars.com | Active in {n_servers} servers"))#Set the presence again
 
     await guild.text_channels[0].send(embed=embedVar)
 @client.event
@@ -46,7 +46,7 @@ async def on_guild_remove(guild):
     global n_servers
     print(f"I was removed from {guild.name} (id: {guild.id})")
     n_servers -= 1
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=f" codewars.com | Active in {n_servers}"))#Set the presence again
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=f" codewars.com | Active in {n_servers} servers"))#Set the presence again
 @client.event
 async def on_message(message):
     args = message.content.split(' ')
